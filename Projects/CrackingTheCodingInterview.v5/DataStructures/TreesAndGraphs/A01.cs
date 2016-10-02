bfa68a1e599b3e3fe550c4e7af3b90f235890978
Onur EKER <onur.eker@gmail.com>
2016-10-02 15:47:09 +0300

    A01 is solved

--- Projects/CrackingTheCodingInterview.v5/DataStructures/TreesAndGraphs/A01.cs
+++ Projects/CrackingTheCodingInterview.v5/DataStructures/TreesAndGraphs/A01.cs
@@ -4,14 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Common;
+using Common.DataStructures.Trees;
 
 namespace CrackingTheCodingInterview.v5.DataStructures.TreesAndGraphs
 {
+    //Implement a function to check if a binary tree is balanced. For the purposes of this
+    //question, a balanced tree is defined to be a tree such that the heights of the two
+    //subtrees of any node never differ by more than one
     class A01: IProgram
     {
         public void Run(string[] args)
         {
-            
+            var root = Trees.Parse("3(4(2), 1)");
+            int height = Height(root);
+            string result = height != -1 ? "BALANCED" : "UNBALANCED";
+            Console.WriteLine(result);
+        }
+
+        private int Height(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var leftHeight = Height(root.Left);
+            if (leftHeight == -1)
+            {
+                return -1;
+            }
+
+            var rightHeight = Height(root.Right);
+            if (rightHeight == -1)
+            {
+                return -1;
+            }
+
+            var diff = Math.Abs(leftHeight - rightHeight);
+            if (diff > 1)
+            {
+                return -1;
+            }
+
+            var result = Math.Max(leftHeight, rightHeight) + 1;
+            return result;
         }
     }
 }
