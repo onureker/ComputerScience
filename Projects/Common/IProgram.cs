0a84fbc4e0380ccd5589f905426de305518c190c
Onur EKER <onur.eker@gmail.com>
2016-10-02 15:21:14 +0300

    A04 solved

--- Projects/Common/IProgram.cs
+++ Projects/Common/IProgram.cs
@@ -1,6 +1,8 @@
 ﻿namespace Common
 {
     //TODO: Bu sieye bak: https://knaidu.gitbooks.io/problem-solving/content/stacks_and_queues/sliding_window_max.html
+    //TODO: Bu kitaba bak: https://www.amazon.com/Elements-Programming-Interviews-Insiders-Guide/dp/1479274836
+    //TODO: http://codeforces.com/
     public interface IProgram
     {
         void Run(string[] args);
--- Projects/HackerRank/DataStructures/Queues/A04.cs
+++ Projects/HackerRank/DataStructures/Queues/A04.cs
@@ -11,6 +11,49 @@ namespace HackerRank.DataStructures.Queues
     //https://www.hackerrank.com/challenges/queries-with-fixed-length
     class A04 : IProgram
     {
+        public class CustomQueue
+        {
+            private readonly IList<int> store;
+
+            public CustomQueue(int size)
+            {
+                store = new List<int>(size);
+            }
+
+            public void Enqueue(int value)
+            {
+                if (store.Count == 0 || value > store[0])
+                {
+                    store.Clear();
+                    store.Add(value);
+                    return;
+                }
+
+                for (int i = store.Count-1; i >= 0; i--)
+                {
+                    if (value > store[i])
+                    {
+                        store.RemoveAt(i);
+                    }
+                }
+
+                store.Add(value);
+            }
+
+            public int Dequeue()
+            {
+                var result = Peek();
+                store.RemoveAt(0);
+                return result;
+            }
+
+            public int Peek()
+            {
+                var result = store[0];
+                return result;
+            }
+        }
+
         public void Run(string[] args)
         {
             /*
@@ -26,12 +69,12 @@ namespace HackerRank.DataStructures.Queues
             }
             */
 
-            //var values = ArrayBuilder<int>.New(3, 2, 1, 4, 5);
+            var values = ArrayBuilder<int>.New(3, 2, 1, 4, 5);
             //var values = ArrayBuilder<int>.New(1, 2, 3, 4, 5);
-            //var d = 2;
+            var d = 2;
 
-            var values = ArrayBuilder<int>.New(176641, 818878, 590130, 846132, 359913, 699520, 974627, 806346, 343832, 619769, 760242, 693331, 832192, 775549, 353117, 23950, 496548, 183204, 971799, 393071, 727476, 351337, 811496, 24595, 417701, 664960, 745806, 538176, 230403, 942316, 21481, 605695, 598531, 651683, 558460, 583357, 530911, 721611, 308228, 724620, 429167, 909353, 330152, 116815, 986067, 713467, 906132, 428600, 927889, 567272, 647109, 992614, 747948, 192884, 879696, 262543, 782487, 829272, 470060, 427956, 751730, 597177, 870616, 754791, 421830, 11676, 425656, 841955, 693419, 462693, 245403, 192649, 750201, 180732, 17450, 44723, 527618, 174579, 515786, 444844, 210843, 563425, 809540, 752036, 608529, 748313, 667439, 255643, 387412, 320353, 704213, 755272, 267902, 657989, 651762, 325654, 582887, 382501, 715426, 897450);
-            var d = 78;
+            //var values = ArrayBuilder<int>.New(176641, 818878, 590130, 846132, 359913, 699520, 974627, 806346, 343832, 619769, 760242, 693331, 832192, 775549, 353117, 23950, 496548, 183204, 971799, 393071, 727476, 351337, 811496, 24595, 417701, 664960, 745806, 538176, 230403, 942316, 21481, 605695, 598531, 651683, 558460, 583357, 530911, 721611, 308228, 724620, 429167, 909353, 330152, 116815, 986067, 713467, 906132, 428600, 927889, 567272, 647109, 992614, 747948, 192884, 879696, 262543, 782487, 829272, 470060, 427956, 751730, 597177, 870616, 754791, 421830, 11676, 425656, 841955, 693419, 462693, 245403, 192649, 750201, 180732, 17450, 44723, 527618, 174579, 515786, 444844, 210843, 563425, 809540, 752036, 608529, 748313, 667439, 255643, 387412, 320353, 704213, 755272, 267902, 657989, 651762, 325654, 582887, 382501, 715426, 897450);
+            //var d = 78;
 
             int result = FindMinOfMaxs(values, d);
             Console.WriteLine(result);
@@ -39,60 +82,26 @@ namespace HackerRank.DataStructures.Queues
 
         private int FindMinOfMaxs(int[] values, int size)
         {
-            Queue<int> max1 = new Queue<int>();
-            Queue<int> max2 = new Queue<int>();
-            values.Take(size).ToList().ForEach(i => Enqueue(i, max1, max2));
-            int min = max1.Peek();
+            CustomQueue customQueue = new CustomQueue(size);
+            values.Take(size).ToList().ForEach(i => customQueue.Enqueue(i));
+            int min = customQueue.Peek();
 
             for (int i = size; i < values.Length; i++)
             {
                 var left = values[i - size];
-                Dequeue(left, max1, max2);
+                if (left == customQueue.Peek())
+                {
+                    customQueue.Dequeue();
+                }
 
                 var right = values[i];
-                Enqueue(right, max1, max2);
+                customQueue.Enqueue(right);
 
-                min = this.Min(min, max1.Peek());
+                min = this.Min(min, customQueue.Peek());
             }
 
             return min;
         }
 
-        private void Dequeue(int value, Queue<int> max1, Queue<int> max2)
-        {
-            if (max1.Count == 0 || value != max1.Peek())
-            {
-                return;
-            }
-
-            max1.Dequeue();
-
-            if (max2.Count == 0)
-            {
-                return;
-            }
-
-            var transferred = max2.Dequeue();
-            max1.Enqueue(transferred);
-        }
-
-        private void Enqueue(int value, Queue<int> max1, Queue<int> max2)
-        {
-            int max1Value = max1.Count == 0 ? int.MinValue : max1.Peek();
-            if (value > max1Value)
-            {
-                max1.Clear();
-                max2.Clear();
-                max1.Enqueue(value);
-                return;
-            }
-
-            int max2Value = max2.Count == 0 ? int.MinValue : max2.Peek();
-            if (value > max2Value)
-            {
-                max2.Clear();
-                max2.Enqueue(value);
-            }
-        }
     }
 }
