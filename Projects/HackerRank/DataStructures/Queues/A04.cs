b3e538686e458405dcecb5243f95b71a34b00a51
Onur EKER <onur.eker@gmail.com>
2016-10-02 12:10:20 +0300

    Solution Try 2

--- Projects/HackerRank/DataStructures/Queues/A04.cs
+++ Projects/HackerRank/DataStructures/Queues/A04.cs
@@ -26,11 +26,12 @@ namespace HackerRank.DataStructures.Queues
             }
             */
 
-            var values = ArrayBuilder<int>.New(3, 2, 1, 4, 5);
-            var d = 2;
+            //var values = ArrayBuilder<int>.New(3, 2, 1, 4, 5);
+            //var values = ArrayBuilder<int>.New(1, 2, 3, 4, 5);
+            //var d = 2;
 
-            //var values = ArrayBuilder<int>.New(176641, 818878, 590130, 846132, 359913, 699520, 974627, 806346, 343832, 619769, 760242, 693331, 832192, 775549, 353117, 23950, 496548, 183204, 971799, 393071, 727476, 351337, 811496, 24595, 417701, 664960, 745806, 538176, 230403, 942316, 21481, 605695, 598531, 651683, 558460, 583357, 530911, 721611, 308228, 724620, 429167, 909353, 330152, 116815, 986067, 713467, 906132, 428600, 927889, 567272, 647109, 992614, 747948, 192884, 879696, 262543, 782487, 829272, 470060, 427956, 751730, 597177, 870616, 754791, 421830, 11676, 425656, 841955, 693419, 462693, 245403, 192649, 750201, 180732, 17450, 44723, 527618, 174579, 515786, 444844, 210843, 563425, 809540, 752036, 608529, 748313, 667439, 255643, 387412, 320353, 704213, 755272, 267902, 657989, 651762, 325654, 582887, 382501, 715426, 897450);
-            //var d = 78;
+            var values = ArrayBuilder<int>.New(176641, 818878, 590130, 846132, 359913, 699520, 974627, 806346, 343832, 619769, 760242, 693331, 832192, 775549, 353117, 23950, 496548, 183204, 971799, 393071, 727476, 351337, 811496, 24595, 417701, 664960, 745806, 538176, 230403, 942316, 21481, 605695, 598531, 651683, 558460, 583357, 530911, 721611, 308228, 724620, 429167, 909353, 330152, 116815, 986067, 713467, 906132, 428600, 927889, 567272, 647109, 992614, 747948, 192884, 879696, 262543, 782487, 829272, 470060, 427956, 751730, 597177, 870616, 754791, 421830, 11676, 425656, 841955, 693419, 462693, 245403, 192649, 750201, 180732, 17450, 44723, 527618, 174579, 515786, 444844, 210843, 563425, 809540, 752036, 608529, 748313, 667439, 255643, 387412, 320353, 704213, 755272, 267902, 657989, 651762, 325654, 582887, 382501, 715426, 897450);
+            var d = 78;
 
             int result = FindMinOfMaxs(values, d);
             Console.WriteLine(result);
@@ -38,29 +39,60 @@ namespace HackerRank.DataStructures.Queues
 
         private int FindMinOfMaxs(int[] values, int size)
         {
-            Stack<int> maxs = new Stack<int>();
-            int min = values[0];
-            maxs.Push(int.MinValue);
-            maxs.Push(min);
+            Queue<int> max1 = new Queue<int>();
+            Queue<int> max2 = new Queue<int>();
+            values.Take(size).ToList().ForEach(i => Enqueue(i, max1, max2));
+            int min = max1.Peek();
 
-            for (int i = 1; i < values.Length; i++)
+            for (int i = size; i < values.Length; i++)
             {
-                var left = i - size < 0 ? int.MaxValue : values[i - size];
-                if (left == maxs.Peek())
-                {
-                    maxs.Pop();
-                }
+                var left = values[i - size];
+                Dequeue(left, max1, max2);
 
                 var right = values[i];
-                if (right > maxs.Peek())
-                {
-                    maxs.Push(right);
-                }
+                Enqueue(right, max1, max2);
 
-                min = this.Min(min, maxs.Peek());
+                min = this.Min(min, max1.Peek());
             }
 
             return min;
+        }
+
+        private void Dequeue(int value, Queue<int> max1, Queue<int> max2)
+        {
+            if (max1.Count == 0 || value != max1.Peek())
+            {
+                return;
+            }
+
+            max1.Dequeue();
+
+            if (max2.Count == 0)
+            {
+                return;
+            }
+
+            var transferred = max2.Dequeue();
+            max1.Enqueue(transferred);
+        }
+
+        private void Enqueue(int value, Queue<int> max1, Queue<int> max2)
+        {
+            int max1Value = max1.Count == 0 ? int.MinValue : max1.Peek();
+            if (value > max1Value)
+            {
+                max1.Clear();
+                max2.Clear();
+                max1.Enqueue(value);
+                return;
+            }
+
+            int max2Value = max2.Count == 0 ? int.MinValue : max2.Peek();
+            if (value > max2Value)
+            {
+                max2.Clear();
+                max2.Enqueue(value);
+            }
         }
     }
 }
