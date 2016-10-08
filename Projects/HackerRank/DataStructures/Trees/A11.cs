b89db80562702d242f144be7f57a3bac202d9072
Onur EKER <onur.eker@gmail.com>
2016-10-08 10:14:20 +0300

    HackerRank A11 is solved

---
+++ Projects/HackerRank/DataStructures/Trees/A11.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Common.DataStructures.Trees;
+
+namespace HackerRank.DataStructures.Trees
+{
+    //Is This a Binary Search Tree?
+    //https://www.hackerrank.com/challenges/is-binary-search-tree
+    class A11 : IProgram
+    {
+        public void Run(string[] args)
+        {
+            var root = TreeBuilder.Parse("3(5(1,4),2(6))");
+            var result = CheckBST(root, int.MinValue, int.MaxValue);
+            Console.WriteLine(result);
+        }
+
+        public bool CheckBST(Node root, int min, int max)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            var current = root.data;
+            if (current <= min || current >= max)
+            {
+                return false;
+            }
+
+            var leftOk = CheckBST(root.left, min, current);
+            if (!leftOk)
+            {
+                return false;
+            }
+
+            var rightOk = CheckBST(root.right, current, max);
+            if (!rightOk)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
