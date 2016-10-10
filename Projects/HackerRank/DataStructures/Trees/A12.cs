a4d1d7bbe604471d7474138cdf3f3297f3cd7c67
Onur EKER <onur.eker@gmail.com>
2016-10-10 23:33:53 +0300

    A12 try

--- Projects/HackerRank/DataStructures/Trees/A12.cs
+++ Projects/HackerRank/DataStructures/Trees/A12.cs
@@ -16,64 +16,78 @@ namespace HackerRank.DataStructures.Trees
         public void Run(string[] args)
         {
             var textReader = Console.In;
-            //int left = textReader.ReadLine().ParseToIntArray()[0];
-            //int right = textReader.ReadLine().ParseToIntArray()[0];
-            int left = 42;
-            int right = 1024;
-            //int left = 1;
-            //int right = 10;
+            //int from = textReader.ReadLine().ParseToIntArray()[0];
+            //int to = textReader.ReadLine().ParseToIntArray()[0];
+            //int from = 42;
+            //int to = 1024;
+            int left = 0;
+            int right = 10;
 
-            var sequence = GetSequence(left, right);
-            Console.WriteLine(sequence.Count);
-            sequence.Select(tuple => tuple.Item1 + " " + tuple.Item2).ToList().ForEach(Console.WriteLine);
+            var upSequence = GetSequenceToUp(left, right);
+            var pivot = (int)Math.Pow(10, upSequence.Last().Item1+1);
+            var downSequence = GetSequenceToDown(right, pivot);
+
+            List<Tuple<int, int>> result = new List<Tuple<int, int>>();
+            result.AddRange(upSequence);
+            result.AddRange(downSequence.Reverse());
+
+            Console.WriteLine(result.Count);
+            result.Select(tuple => tuple.Item1 + " " + tuple.Item2).ToList().ForEach(Console.WriteLine);
         }
 
-        private IList<Tuple<int, int>> GetSequence(int left, int right)
+        private IList<Tuple<int, int>> GetSequenceToDown(int from, int to)
         {
-            IList<Tuple<int, int>> sequence = new List<Tuple<int, int>>();
-
-            int level = 0;
-            int current = left - 1;
-            while (true)
+            int level = GetLevel(from);
+            var step = (int)Math.Pow(10, level);
+            var upper = (int)Math.Pow(10, level + 1);
+            if (upper > to)
             {
-                int multiplier = (int)Math.Pow(10, level);
-                int levelRelativeValue = current/multiplier;
-                int remaining = levelRelativeValue % 10;
-
-                if (remaining == 0)
-                {
-                    level++;
-                    break;
-                }
-
-                int needed = 10 - remaining;
-
-                var tuple = new Tuple<int, int>(level, needed);
-                sequence.Add(tuple);
-
-                int increase = needed*multiplier;
-                current += increase;
-                level++;
+                return new List<Tuple<int, int>>();
             }
 
-            current = right - current;
-            while (current != 0)
-            {
-                int multiplier = (int)Math.Pow(10, level);
-                int levelRelativeValue = current / multiplier;
-
-                int remaining = levelRelativeValue % 10;
-
-                var tuple = new Tuple<int, int>(level, remaining);
-                sequence.Add(tuple);
-
-                int decrease = remaining * multiplier;
-                current -= decrease;
-
-                level--;
-            }
+            int count = from % upper / step;
+            var sequence = GetSequenceToDown(from - count * step, to);
+            var tuple = new Tuple<int, int>(level, count);
+            sequence.Insert(0, tuple);
 
             return sequence;
+        }
+
+        private IList<Tuple<int, int>> GetSequenceToUp(int from, int to)
+        {
+            int level = GetLevel(from);
+            var step = (int) Math.Pow(10, level);
+            var upper = (int) Math.Pow(10, level + 1);
+            if (step > to-from)
+            {
+                return new List<Tuple<int, int>>();
+            }
+
+            int target = to > upper ? upper : to;
+            int count = (target - from) / step;
+            var sequence = GetSequenceToUp(@from+ count*step, to);
+            var tuple = new Tuple<int, int>(level, count);
+            sequence.Insert(0, tuple);
+
+            return sequence;
+        }
+
+        private int GetLevel(int value)
+        {
+            if (value == 0)
+            {
+                return 0;
+            }
+
+            int level = 0;
+            int current = value;
+            while (current % 10 == 0)
+            {
+                level++;
+                current = current/10;
+            }
+
+            return level;
         }
     }
 }
